4416646bc347edb6e812a63189103225fc3d0e57
Alaa-Al-Googa <alaaalgooga@gmail.com>
2025-07-31 14:24:30 +0300

    six

---
+++ lib/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubitCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubitCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
---
+++ lib/features/home/presentation/manger/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,22 @@
+part of 'newest_books_cubit.dart';
+
+abstract class NewestBooksState extends Equatable {
+  const NewestBooksState();
+  @override
+  List<Object> get props => [];
+}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+class NewestBooksLoading extends NewestBooksState {}
+
+class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  NewestBooksFailure(this.errMessage);
+}
+
+class NewestBooksSuccess extends NewestBooksState {
+  final List<BookModel> books;
+  NewestBooksSuccess(this.books);
+}
